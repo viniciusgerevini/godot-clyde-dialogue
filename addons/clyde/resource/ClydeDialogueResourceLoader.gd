4e169345d559a7f89aa1bc589e56381c6e467296
Todor Imreorov <blurymind@gmail.com>
2026-08-10 23:52:23 +0100

    Allow clyde to load clyde files outside of res:// (#75)
    Allow clyde to load resources from any absolute path on the file system - not just the resources folder

--- addons/clyde/resource/ClydeDialogueResourceLoader.gd
+++ addons/clyde/resource/ClydeDialogueResourceLoader.gd
@@ -1,5 +1,6 @@
 @tool
 class_name ClydeDialogueResourceLoader extends ResourceFormatLoader
+const Parser = preload("../parser/Parser.gd")
 
 func _get_recognized_extensions() -> PackedStringArray:
 	return PackedStringArray(["clyde", "res"])
@@ -17,4 +18,11 @@ func _handles_type(type: StringName) -> bool:
 
 
 func _load(path, original_path, use_sub_threads, cache_mode):
+	if !path.begins_with("res://"):
+		var file = FileAccess.open(path, FileAccess.READ)
+		var clyde = file.get_as_text()
+		var container = ClydeDialogueFile.new()
+		var parser = Parser.new()
+		container.content = parser.parse(clyde)
+		return container
 	return ResourceLoader.load(path)
